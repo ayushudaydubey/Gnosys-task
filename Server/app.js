e8447156984bc9d4638615b280c1139aa6ddc06e
ayushudaydubey <102001ayushdubey@gmail.com>
2026-04-21 18:28:15 +0530

    frontend build

--- Server/app.js
+++ Server/app.js
@@ -6,15 +6,22 @@ import  candidateRoutes from './routes/candidate.routes.js'
 import screeningRoutes  from  './routes/screening.routes.js'
 import roundRoutes from './routes/round.routes.js'
 import dashboardRoutes from  './routes/dashboard.routes.js'
+import path from 'path'
  
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+
+app.use(express.static(path.join(__dirname,"../public")))
 
 app.use("/api/candidates", candidateRoutes);
 app.use("/api/screening", screeningRoutes);
 app.use("/api/rounds", roundRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+
+app.get("*name",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
 
 app.use(cors({
   origin: "http://localhost:5173", 
